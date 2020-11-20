7e718716bf91475c72bfd6a009cf4a8cdf6b09ac
Gabriel Fair <2096785+gabefair@users.noreply.github.com>
2020-11-20 09:22:35 -0500

    Create raspberry_start.sh

---
+++ raspberry_start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DISPLAY=:0 /usr/bin/python3 /home/daoi-allo/Desktop/code/quick_py_method.py "/usr/lib/chromium-browser/chromium-browser" /home/daoi-allo/Desktop/code/input_links 1 > /home/daoi-allo/Desktop/rc-local_quick_py_method.output
+exit 0
